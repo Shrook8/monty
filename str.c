385509625502b021d19c7f27bdf381b61eadfb8f
shrook <shrookalattar8@gmail.com>
2023-12-14 15:43:10 -0800

    str

---
+++ str.c
@@ -0,0 +1,98 @@
+#include "mo.h"
+
+/**
+ * print_char - print ASCII 
+ * @stack: pointer
+ * @line_number: int num of the line
+*/
+
+void print_char(stack_t **stack, unsigned int line_number)
+{
+	int ascii;
+
+	if (stack == NULL || *stack == NULL)
+		string_err(11, line_number);
+
+	ascii = (*stack)->n;
+	if (ascii < 0 || ascii > 127)
+		string_err(10, line_number);
+	printf("%c\n", ascii);
+}
+
+/**
+ * print_str - print the str
+ * @stack: pointer
+ * @ln: int num of the line
+*/
+
+void print_str(stack_t **stack, __attribute__((unused))unsigned int ln)
+{
+	int ascii;
+	stack_t *tmp;
+
+	if (stack == NULL || *stack == NULL)
+	{
+		printf("\n");
+		return;
+	}
+
+	tmp = *stack;
+	while (tmp != NULL)
+	{
+		ascii = tmp->n;
+		if (ascii <= 0 || ascii > 127)
+			break;
+		printf("%c", ascii);
+		tmp = tmp->next;
+	}
+	printf("\n");
+}
+
+/**
+ * rotl - rotates the first node
+ * @stack: pointer
+ * @ln: int num of the line
+*/
+
+void rotl(stack_t **stack, __attribute__((unused))unsigned int ln)
+{
+	stack_t *tmp;
+
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+		return;
+
+	tmp = *stack;
+	while (tmp->next != NULL)
+		tmp = tmp->next;
+
+	tmp->next = *stack;
+	(*stack)->prev = tmp;
+	*stack = (*stack)->next;
+	(*stack)->prev->next = NULL;
+	(*stack)->prev = NULL
+}
+
+/**
+ * rotr - rotates the last node
+ * @stack: pointer
+ * @ln: int num of the line
+*/
+
+void rotr(stack_t **stack, __attribute__((unused))unsigned int ln)
+{
+	stack_t *tmp;
+
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+		return;
+
+	tmp = *stack;
+
+	while (tmp->next != NULL)
+		tmp = tmp->next;
+
+	tmp->next = *stack;
+	tmp->prev->next = NULL;
+	tmp->prev- = NULL;
+	(*stack)->prev = tmp;
+	(*stack) = tmp;
+}
