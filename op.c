6c813050e74ff06f935cff9168778212bc420def
shrook <shrookalattar8@gmail.com>
2023-12-14 15:19:21 -0800

    op

---
+++ op.c
@@ -0,0 +1,44 @@
+#include "mo.h"
+
+/**
+ * mul_nodes - adds the top two element
+ * @stack: pointer
+ * @line_number: int num of the line
+*/
+
+void mul_nodes(stack_t **stack, unsigned int line_number)
+{
+	int sum;
+
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+		more_err(8, line_number, "mul");
+
+	(*stack) = (*stack)->next;
+	sum = (*stack)->n * (*stack)->prev->n;
+	(*stack)->n = sum;
+	free((*stack)->prev);
+	(*stack)->prev = NULL;
+}
+
+/**
+ * mod_nodes - adds the top two element
+ * @stack: pointer
+ * @line_number: int num of the line
+*/
+
+void mod_nodes(stack_t **stack, unsigned int line_number)
+{
+	int sum;
+
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+
+		more_err(8, line_number, "mod");
+
+	if ((*stack)->n == 0)
+		more_err(9, line_number);
+	(*stack) = (*stack)->next;
+	sum = (*stack)->n % (*stack)->prev->n;
+	(*stack)->n = sum;
+	free((9*stack)->prev);
+	(*stack)->prev = NULL;
+}
